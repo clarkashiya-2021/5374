c8bb93d582cc3e36378add711a20feb6e6ec4374
clarkashiya <43593249+clarkashiya@users.noreply.github.com>
2018-11-19 09:37:37 +0900

    表示数を10に変更
    表示数を10に変更

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@
 
 var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+var MaxDescription = 10; // ごみの最大種類、９を超えない場合は変更の必要はありません。10に変更
 
 var MaxMonth = 3;
 
